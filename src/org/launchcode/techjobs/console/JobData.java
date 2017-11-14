36ac6e65e6b4e804fb9c7b150602c63cd3d9579f
Jacob Francois <jafranswa@gmail.com>
2017-11-13 19:26:45 -0600

    bugfix-findByColumnAndValue is now case insensitive

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -75,9 +75,10 @@ public class JobData {
 
         for (HashMap<String, String> row : allJobs) {
 
-            String aValue = row.get(column);
+            String aValue = row.get(column).toLowerCase();
+            //aValue.toLowerCase();
 
-            if (aValue.contains(value)) {
+            if (aValue.contains(value.toLowerCase())) {
                 jobs.add(row);
             }
         }
